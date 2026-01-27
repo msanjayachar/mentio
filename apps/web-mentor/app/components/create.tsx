020f930e01e78c178ae468e9d73f7163bb9927bf
Sanjay Achar <m.sanjayachar@gmail.com>
2026-01-27 20:36:22 +0530

    ui: update features SVG icons

--- apps/web-mentor/app/components/create.tsx
+++ apps/web-mentor/app/components/create.tsx
@@ -13,8 +13,8 @@ const Create = () => {
         <span className="3xl:block hidden">3xl</span>
       </div>
       <h1 className="mb-12 px-4 text-4xl">Welcome!</h1>
-      <div className="flex flex-col gap-4 px-4 md:flex-row">
-        <button className="flex h-10 w-full items-center justify-between rounded-full border-2 border-gray-600 bg-black px-4 text-center text-sm text-white md:w-48">
+      <div className="flex flex-col gap-4 px-4 sm:flex-row">
+        <button className="flex h-10 w-full items-center justify-between rounded-full border-2 border-gray-600 bg-black px-4 text-center text-sm text-white sm:w-44">
           <span className="mx-auto">New Menti</span>
           <span className="flex h-full cursor-pointer items-center border-l border-gray-400 py-1 pl-1.5">
             <ChevronDown />
--- apps/web-mentor/app/components/features.tsx
+++ apps/web-mentor/app/components/features.tsx
@@ -1,60 +1,83 @@
+import Image from "next/image";
+
 const Features = () => {
   return (
     <div className="mt-8 flex h-80 max-w-[1570px] flex-col gap-4 rounded-lg py-8">
-      <div className="flex flex-wrap justify-between gap-4 rounded-xl bg-blue-100 px-8 py-8 text-black md:justify-start xl:justify-between">
-        <h1 className="w-full pl-8 font-medium text-black">Popular features</h1>
+      <div className="flex flex-wrap justify-start gap-4 rounded-xl bg-blue-100 px-8 py-8 text-black sm:flex-nowrap sm:justify-between md:justify-start xl:justify-between">
+        {/* TODO: Fix this  */}
+        {/* <h1 className="w-full pl-8 font-medium text-black">Popular features</h1> */}
         {/* Feature One */}
         <div className="flex cursor-pointer flex-col items-center gap-2">
-          <div className="h-44 w-44 rounded-lg border-2 border-transparent bg-gray-100 hover:border-blue-900">
-            <div className="mt-8 ml-4 flex">
-              <div className="ml-6 h-16 w-16 rounded-full bg-red-400" />
-              <div className="-ml-6 h-16 w-16 rounded-full bg-red-400" />
-            </div>
-            <div className="-mt-4 ml-4 flex">
-              <div className="ml-6 h-16 w-16 rounded-full bg-red-400" />
-              <div className="-ml-6 h-16 w-16 rounded-full bg-red-400" />
-            </div>
+          <div className="size-32 rounded-lg border-2 border-transparent bg-gray-100 hover:border-blue-900 md:size-44">
+            <Image
+              src="/features/word_count.svg"
+              alt="testing svg images"
+              width={1000}
+              height={1000}
+            />
           </div>
           <span className="text-sm font-light">Word cloud</span>
         </div>
+
         <div className="flex cursor-pointer flex-col items-center gap-2">
-          <div className="flex h-44 w-44 scale-y-[-1] rounded-lg border-2 border-transparent bg-gray-100 p-8 hover:border-blue-900">
-            <div className="h-12 w-8 bg-blue-500" />
-            <div className="h-20 w-8 bg-blue-500" />
-            <div className="h-28 w-8 bg-blue-500" />
+          <div className="flex size-32 rounded-lg border-2 border-transparent bg-gray-100 p-8 hover:border-blue-900 md:size-44">
+            <Image
+              src="/features/poll.svg"
+              alt="testing svg images"
+              width={100}
+              height={100}
+            />
           </div>
           <span className="text-sm font-light">Poll</span>
         </div>
+
         <div className="flex cursor-pointer flex-col items-center gap-2">
-          <div className="relative h-44 w-44 rounded-lg border-2 border-transparent bg-gray-100 p-10 hover:border-blue-900">
-            <div className="h-24 w-24 rounded-full bg-red-300" />
-            <div className="absolute inset-0 top-10 left-10 h-24 w-10 bg-red-300" />
+          <div className="relative size-32 rounded-lg border-2 border-transparent bg-gray-100 p-10 hover:border-blue-900 md:size-44">
+            <Image
+              src="/features/open_ended.svg"
+              alt="testing svg images"
+              width={1000}
+              height={1000}
+              sizes="2"
+            />
           </div>
           <span className="text-sm font-light">Open Ended</span>
         </div>
 
         <div className="flex cursor-pointer flex-col items-center gap-2">
-          <div className="h-44 w-44 rounded-lg border-2 border-transparent bg-gray-100 p-10 px-16 hover:border-blue-900">
-            <div className="h-16 w-12 bg-blue-400" />
-            <div className="h-8 w-12 bg-red-400" />
+          <div className="flex size-32 items-center justify-center rounded-lg border-2 border-transparent bg-gray-100 p-10 hover:border-blue-900 md:size-44">
+            <Image
+              src="/features/scales.svg"
+              alt="testing svg images"
+              width={500}
+              height={500}
+            />
           </div>
           <span className="text-sm font-light">Scales</span>
         </div>
 
         <div className="flex cursor-pointer flex-col items-center gap-2">
-          <div className="h-44 w-44 rounded-lg border-2 border-transparent bg-gray-100 hover:border-blue-900">
-            <div className="flex scale-x-[-1] rounded-lg bg-gray-100 p-7">
-              <div className="h-12 w-8 bg-green-600" />
-              <div className="h-20 w-8 bg-green-600" />
-              <div className="h-28 w-8 bg-green-600" />
+          <div className="size-32 rounded-lg border-2 border-transparent bg-gray-100 hover:border-blue-900 md:size-44">
+            <div className="mt-5 flex scale-x-[-1] rounded-lg bg-gray-100 p-7">
+              <Image
+                src="/features/rankings.svg"
+                alt="testing svg images"
+                width={1000}
+                height={1000}
+              />
             </div>
           </div>
           <span className="text-sm font-light">Ranking</span>
         </div>
 
-        <div className="flex cursor-pointer flex-col items-center gap-2">
-          <div className="flex h-44 w-44 items-center justify-center rounded-lg border-2 border-transparent bg-gray-100 hover:border-blue-900">
-            <div className="h-12 w-12 bg-red-400"></div>
+        <div className="flex cursor-pointer flex-col items-center gap-2 sm:hidden lg:flex">
+          <div className="flex size-32 items-center justify-center rounded-lg border-2 border-transparent bg-gray-100 pb-6 hover:border-blue-900 md:size-44">
+            <Image
+              src="/features/pin.svg"
+              alt="testing svg images"
+              width={1000}
+              height={1000}
+            />
           </div>
           <span className="text-sm font-light">Pin on Image</span>
         </div>
