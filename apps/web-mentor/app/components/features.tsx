6715454e350b522bf7e5f47a13e074c824ea102e
Sanjay Achar <m.sanjayachar@gmail.com>
2026-01-28 17:13:10 +0530

    ui: make features section responsive

--- apps/web-mentor/app/components/features.tsx
+++ apps/web-mentor/app/components/features.tsx
@@ -3,12 +3,12 @@ import Image from "next/image";
 const Features = () => {
   return (
     <div className="mt-8 ml-4 flex h-80 w-auto max-w-[1500px] flex-col gap-4 rounded-lg py-8">
-      <div className="flex flex-wrap justify-start gap-4 rounded-xl bg-blue-100 px-8 py-8 text-black sm:flex-nowrap sm:justify-between md:justify-start lg:justify-between xl:justify-between">
+      <div className="flex flex-wrap items-center justify-start gap-10 rounded-xl bg-blue-100 px-8 py-8 text-black sm:flex-nowrap sm:justify-between md:justify-start lg:justify-between">
         {/* TODO: Fix this  */}
         {/* <h1 className="w-full pl-8 font-medium text-black">Popular features</h1> */}
         {/* Feature One */}
         <div className="flex cursor-pointer flex-col items-center gap-2">
-          <div className="size-30 rounded-lg border-2 border-transparent bg-gray-100 hover:border-blue-900 sm:size-22 md:h-auto md:w-auto md:max-w-40 lg:max-h-36 lg:max-w-36 2xl:max-h-44 2xl:max-w-44">
+          <div className="size-28 rounded-lg border-2 border-transparent bg-gray-100 hover:border-blue-900 sm:size-22 md:h-auto md:w-auto md:max-w-40 lg:max-h-36 lg:max-w-36 2xl:max-h-44 2xl:max-w-44">
             <Image
               src="/features/word_count.svg"
               alt="testing svg images"
@@ -21,7 +21,7 @@ const Features = () => {
 
         {/* Feature Two */}
         <div className="flex cursor-pointer flex-col items-center gap-2">
-          <div className="flex size-30 rounded-lg border-2 border-transparent bg-gray-100 hover:border-blue-900 sm:size-22 md:h-auto md:w-auto md:max-w-40 lg:max-h-36 lg:max-w-36 2xl:max-h-44 2xl:max-w-44">
+          <div className="flex size-28 rounded-lg border-2 border-transparent bg-gray-100 hover:border-blue-900 sm:size-22 md:h-auto md:w-auto md:max-w-40 lg:max-h-36 lg:max-w-36 2xl:max-h-44 2xl:max-w-44">
             <Image
               src="/features/poll_two.svg"
               alt="testing svg images"
@@ -34,7 +34,7 @@ const Features = () => {
 
         {/* Feature Three */}
         <div className="flex cursor-pointer flex-col items-center gap-2">
-          <div className="relative size-30 rounded-lg border-2 border-transparent bg-gray-100 hover:border-blue-900 sm:size-22 md:h-auto md:w-auto md:max-w-40 lg:max-h-36 lg:max-w-36 2xl:max-h-44 2xl:max-w-44">
+          <div className="relative size-28 rounded-lg border-2 border-transparent bg-gray-100 hover:border-blue-900 sm:size-22 md:h-auto md:w-auto md:max-w-40 lg:max-h-36 lg:max-w-36 2xl:max-h-44 2xl:max-w-44">
             <Image
               src="/features/open_ended.svg"
               alt="testing svg images"
@@ -47,7 +47,7 @@ const Features = () => {
 
         {/* Feature Four */}
         <div className="flex cursor-pointer flex-col items-center gap-2">
-          <div className="flex size-30 items-center justify-center rounded-lg border-2 border-transparent bg-gray-100 hover:border-blue-900 sm:size-22 md:h-auto md:w-auto md:max-w-40 lg:max-h-36 lg:max-w-36 2xl:max-h-44 2xl:max-w-44">
+          <div className="flex size-28 items-center justify-center rounded-lg border-2 border-transparent bg-gray-100 hover:border-blue-900 sm:size-22 md:h-auto md:w-auto md:max-w-40 lg:max-h-36 lg:max-w-36 2xl:max-h-44 2xl:max-w-44">
             <Image
               src="/features/scales.svg"
               alt="testing svg images"
@@ -60,7 +60,7 @@ const Features = () => {
 
         {/* Feature Five */}
         <div className="flex cursor-pointer flex-col items-center gap-2">
-          <div className="size-30 rounded-lg border-2 border-transparent bg-gray-100 hover:border-blue-900 sm:size-22 md:h-auto md:w-auto md:max-w-40 lg:max-h-36 lg:max-w-36 2xl:max-h-44 2xl:max-w-44">
+          <div className="size-28 rounded-lg border-2 border-transparent bg-gray-100 hover:border-blue-900 sm:size-22 md:h-auto md:w-auto md:max-w-40 lg:max-h-36 lg:max-w-36 2xl:max-h-44 2xl:max-w-44">
             <div className="flex scale-x-[-1] rounded-lg bg-gray-100">
               <Image
                 src="/features/rankings.svg"
@@ -75,7 +75,7 @@ const Features = () => {
 
         {/* Feature Six */}
         <div className="cursor-pointer flex-col items-center gap-2 sm:hidden [@media(min-width:1100px)]:flex">
-          <div className="flex size-30 items-center justify-center rounded-lg border-2 border-transparent bg-gray-100 hover:border-blue-900 sm:size-22 md:h-auto md:w-auto md:max-w-40 lg:max-h-36 lg:max-w-36 2xl:max-h-44 2xl:max-w-44">
+          <div className="flex size-28 items-center justify-center rounded-lg border-2 border-transparent bg-gray-100 hover:border-blue-900 sm:size-22 md:h-auto md:w-auto md:max-w-40 lg:max-h-36 lg:max-w-36 2xl:max-h-44 2xl:max-w-44">
             <Image
               src="/features/pin.svg"
               alt="testing svg images"
--- apps/web-mentor/app/test/page.tsx
+++ apps/web-mentor/app/test/page.tsx
@@ -3,16 +3,6 @@ import { ArrowUp, Sparkles } from "lucide-react";
 export default function Page() {
   return (
     <div>
-      <div className="bg-red-400 sm:bg-blue-300 md:bg-purple-400 lg:bg-blue-500 xl:bg-green-400 2xl:bg-pink-500">
-        <span className="block sm:hidden">base</span>
-        <span className="hidden sm:block md:hidden">sm</span>
-        <span className="hidden md:block lg:hidden">md</span>
-        <span className="hidden lg:block xl:hidden">lg</span>
-        <span className="hidden xl:block 2xl:hidden">xl</span>
-        <span className="3xl:hidden hidden 2xl:block">2xl</span>
-        <span className="3xl:block hidden">3xl</span>
-      </div>
-
       <div className="mx-4">
         <div className="w-auto bg-blue-400">
           <div className="ml-4 flex max-w-[1500px] min-w-0 items-center rounded-lg border-2 border-gray-400">
