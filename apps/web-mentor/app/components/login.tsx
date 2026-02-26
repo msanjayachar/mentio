e57f2d1d0840e096dbe89ed15ac9e7a9711eaeb3
Sanjay Achar <m.sanjayachar@gmail.com>
2026-02-26 17:05:14 +0530

    ui: add profile dropdown on homenavbar. add signup and login page with dedicated components

--- apps/web-mentor/app/components/login.tsx
+++ apps/web-mentor/app/components/login.tsx
@@ -1,5 +1,77 @@
+"use client";
+
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+
 const Login = () => {
-  return <div></div>;
+  const [email, setEmail] = useState(null);
+  const [password, setPassword] = useState(null);
+  const router = useRouter();
+
+  const handleLogin = async () => {
+    const response = await fetch("http://localhost:8000/api/auth/login", {
+      method: "POST",
+      body: JSON.stringify({ email, password }),
+    });
+  };
+
+  return (
+    <div>
+      <div className="flex h-auto w-96 flex-col gap-8 rounded-md bg-blue-100 p-8">
+        <h1 className="text-center text-lg">Create a free account</h1>
+
+        <button className="flex h-8 cursor-pointer items-center justify-center gap-2 rounded-xl border-2 border-black text-xs">
+          <Image
+            src="/icons/google-logo.png"
+            alt="google-logo"
+            width={16}
+            height={16}
+          />
+          <span>Login with Google</span>
+        </button>
+
+        <p className="text-center text-xs font-light text-gray-400">
+          or using email
+        </p>
+
+        <input
+          placeholder="Enter your email here."
+          className="rounded-md border-2 border-gray-300 bg-gray-200 p-2 text-xs font-light hover:border-blue-900"
+        />
+
+        <input
+          placeholder="Enter your password here."
+          className="rounded-md border-2 border-gray-300 bg-gray-200 p-2 text-xs font-light hover:border-blue-900"
+        />
+        <button
+          className="h-10 cursor-pointer rounded-full bg-black text-sm text-white"
+          onClick={() => handleLogin()}
+        >
+          Login
+        </button>
+      </div>
+
+      <div>
+        <p className="mt-4 mb-8 text-center text-[12px] font-light tracking-wide text-[#6D6D6C]">
+          By signing up you accept our terms of use and policies.
+        </p>
+      </div>
+
+      <div className="mt-4 text-center text-lg">
+        <h2>
+          Don&apos;t have an account ?{" "}
+          <span
+            className="cursor-pointer text-blue-900 underline"
+            onClick={() => router.push("/signup")}
+          >
+            Sign up
+          </span>{" "}
+          here.
+        </h2>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
---
+++ apps/web-mentor/app/login/page.tsx
@@ -0,0 +1,11 @@
+import Login from "../components/login";
+
+const page = () => {
+  return (
+    <div className="flex h-screen items-center justify-center bg-gray-100">
+      <Login />
+    </div>
+  );
+};
+
+export default page;
