5f3e4daa357920dff4d68c9582db01d99fc251f0
Sanjay Achar <m.sanjayachar@gmail.com>
2026-03-26 11:59:13 +0530

    feat: add simple socket.io(websockets) server and client connection

--- apps/backend/main.ts
+++ apps/backend/main.ts
@@ -1,7 +1,19 @@
 import { app } from "./app";
+import { Server } from "socket.io";
+import { createServer } from "node:http";
 
 const port = 8000;
+const server = createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3001",
+  },
+});
 
-app.listen(port, () => {
+io.on("connection", (socket) => {
+  console.log("a user connected");
+});
+
+server.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
